8b11be3d981c2b83d11b6382fee7f80d9960dbf8
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-03-08 06:26:09 +0000

    assign.cpp

--- assign.cpp
+++ assign.cpp
@@ -9,53 +9,47 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
-
-int setColor(int color) {
-#ifdef _WIN32
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-#else
-    std::cout << "\033[" << color << "m";
-#endif
-}
+#include <string>
 
 int main() {
-    setColor(34);  // Set text color to blue
-    std::cout << "Welcome to the Torus Calculator!" << std::endl;
-    setColor(37);  // Reset to default color
-
     float majorRadius, minorRadius;
-    std::string unit;
+    std::string units;
 
-    std::cout << "Enter the major radius: ";
-    std::cin >> majorRadius;
-    std::cout << "Enter the minor radius: ";
-    std::cin >> minorRadius;
-    std::cout << "Enter the unit of measurement (meters, centimeters, etc): ";
-    std::cin >> unit;
+    std::cout << "Welcome to the Torus Calculator!" << std::endl;
 
-    float area = 4 * M_PI^2 * majorRadius * minorRadius;
-    float volume = 2 * M_PI^2 * majorRadius * minorRadius * minorRadius;
+    while (true) {
+        std::cout << "Please enter the major radius (R): ";
+        std::cin >> majorRadius;
+        std::cout << "Please enter the minor radius (r): ";
+        std::cin >> minorRadius;
+
+        if (majorRadius > minorRadius) {
+            break;  // the program doesn't go further if conditions aren't met
+        } else {
+            std::cout << "The major radius must be greater than the minor radius. Please input new digits." << std::endl;
+        }
+    }
+
+    std::cout << "Please enter the units (e.g., cm, m): ";
+    std::cin >> units;
+
+    float volume = 2 * M_PI * M_PI * majorRadius * minorRadius * minorRadius;
+    float area = 4 * M_PI * M_PI * majorRadius * minorRadius;
 
     std::cout << std::fixed << std::setprecision(2);
-    std::cout << "Area: " << area << " " << unit << "^2" << std::endl;
-    std::cout << "Volume: " << volume << " " << unit << "^3" << std::endl;
+    std::cout << "The volume of the torus is: " << volume << " " << units << "^3" << std::endl;
+    std::cout << "The surface area of the torus is: " << area << " " << units << "^2" << std::endl;
 
     std::string showFormulas;
     std::cout << "Do you want to see the formulas used? (yes/no): ";
     std::cin >> showFormulas;
 
     if (showFormulas == "yes") {
-        setColor(32);  // Set text color to green
-        std::cout << "Formulas used:" << std::endl;
-        std::cout << "Surface Area = 4π2Rr" << std::endl;
-        std::cout << "Volume = 2π2Rr2" << std::endl;
+        std::cout << "Volume formula= 2π²Rr²" << std::endl;
+        std::cout << "Area formula= 4π²Rr" << std::endl;
     } else {
-        setColor(31);  // Set text color to red
-        std::cout << "Alright, no formulas will be shown." << std::endl;
+        std::cout << "Alright, no codes will be shown." << std::endl;
     }
 
-    setColor(34);  // Set text color to blue
     std::cout << "Thank you for using the Torus Calculator! Goodbye!" << std::endl;
-    setColor(37);  // Reset to default color
-
 }
