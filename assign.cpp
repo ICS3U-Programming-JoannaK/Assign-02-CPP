5385c70c0c04f099f20e097bc610cef856941773
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-03-08 04:48:55 +0000

    assign.cpp

---
+++ assign.cpp
@@ -0,0 +1,28 @@
+// Copyright 2025 Joanna Keza All rights reserved
+//
+// Created by: Joanna Keza
+// Data: March 7, 2025
+// This program asks the user for the major radius
+// and minor radius of a Torus, then calculates
+// the surface area and volume using PI
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+    // Greeting card
+    std::cout << "************************************" << std::endl;
+    std::cout << "*                                  *" << std::endl;
+    std::cout << "* Welcome to the Torus calculator !*" << std::endl;
+    std::cout << "*                                  *" << std::endl;
+    std::cout << "************************************" << std::endl;
+
+    // Declare variables
+    float majorRadius float minorRadius
+
+    // Get the user input for major and minor radius
+    std::cout << "Enter the major radius: ";
+    std::cin >> majorRadius;
+    std::cout << "Enter the minor radius: ";
+    std::cin >> minorRadius;
