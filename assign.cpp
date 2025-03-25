38d021b1b93c07105e1759518e94fdb0794e7055
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-03-25 22:15:37 +0000

    assign.cpp

--- assign.cpp
+++ assign.cpp
@@ -10,7 +10,6 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
-#include <string>
 
 // This displays the greeting card in color
 int color() {
@@ -38,22 +37,13 @@ int main() {
     std::string units;
 
     // Ask user to enter the major and minor radius
-    while (true) {
-        std::cout << "Please enter the major radius (R): ";
-        std::cin >> majorRadius;
-        std::cout << "Please enter the minor radius (r): ";
-        std::cin >> minorRadius;
+    std::cout << "Please enter the major radius (R): ";
+    std::cin >> majorRadius;
+    std::cout << "Please enter the minor radius (r): ";
+    std::cin >> minorRadius;
 
-        // This makes sure the program won't run
-        // if the minor radius is greater than the major radius
-        if (majorRadius > minorRadius) {
-            break;  // the program won't go further if conditions aren't met
-        } else {
-            std::cout << "The major radius must be "
-                      << "greater than minor radius." << std::endl;
-            std::cout << "Please input new digits." << std::endl;
-        }
-    }
+    // Display pointer for future references
+    std::cout << "Make sure next time your major radius is larger than minor radius." << std::endl;
 
     // Get units from the user
     std::cout << "Please enter the units (e.g., cm, m): ";
@@ -70,54 +60,32 @@ int main() {
     std::cout << "The surface area of the torus is: "
               << area << " " << units << "^2" << std::endl;
 
-    // The user is able to see what formulas
-    // were used to calculate the area and volume
-    std::string showFormulas;
-    std::cout << "Do you want to see the formulas used? (yes/no): ";
-    std::cin >> showFormulas;
+    // Display formulas used
+    std::cout << "Volume formula= 2π²Rr²" << std::endl;
+    std::cout << "Area formula= 4π²Rr" << std::endl;
 
-    // The user will only be able to see the formulas if they type yes
-    if (showFormulas == "yes") {
-        std::cout << "Volume formula= 2π²Rr²" << std::endl;
-        std::cout << "Area formula= 4π²Rr" << std::endl;
-    } else {
-        std::cout << "Alright, no codes will be shown." << std::endl;
-        // This will display when the user types no
+    // Display more information about the Torus
+    std::cout << "------More on the Torus shape------\n"
+    << std::endl;
+    std::cout << "A Torus is a 3D shape that resembles \n"
+    << "a donut. It has two radius:\n"
+    << std::endl;
+    std::cout << "1. Major Radius (R): This is the distance from \n"
+    << "the center of hole to the center of the tube\n"
+    << std::endl;
+    std::cout << "Minor radius (r): This is the radius of the tube itself"
+    << std::endl;
+    std::cout << "the distance from the center of the"
+    << "tube to the surface of the Torus\n"
+    << std::endl;
+    std::cout << "Thank you for using the Torus calculator ! Goodbye!\n"
+    << std::endl;
+
+    // Display exit card
+    std::cout << "\033[1;35m";  // the greeting card will be purple !
+    std::cout << "**********************8***************" << std::endl;
+    std::cout << "*                                    *" << std::endl;
+    std::cout << "* Thank you for using the calculator!*" << std::endl;
+    std::cout << "*                                    *" << std::endl;
+    std::cout << "**************************************" << std::endl;
     }
-
-    // Ask the user if they want to know more about the Torus shape
-    std::string displayInfo;
-    std::cout << "Do you want to learn more about the shape? (yes/no): ";
-    std::cin >> displayInfo;
-
-    // The user will only be able to learn more if they type yes
-    if (displayInfo == "yes") {
-        std::cout << "------More on the Torus shape------\n"
-                  << std::endl;
-        std::cout << "A Torus is a 3D shape that resembles"
-                  << "a donut. It has two radius:\n"
-                  << std::endl;
-        std::cout << "1. Major Radius (R): This is the distance from"
-                  << "the center of hole to the center of the tube\n"
-                  << std::endl;
-        std::cout << "Minor radius (r): This is the radius of the tube itself"
-                  << std::endl;
-        std::cout << "the distance from the center of the"
-                  << "tube to the surface of the Torus"
-                  << std::endl;
-        std::cout << "Minor radius (r): This is the radius of the tube itself,"
-                  << std::endl;
-        std::cout << "the distance from the center of"
-                  << "the tube to the surface of the Torus\n"
-                  << std::endl;
-        std::cout << "Thank you for using the Torus calculator ! Goodbye!\n"
-                  << std::endl;
-
-    } else {  // Only displays exit card if user types no
-        std::cout << "**********************8***************" << std::endl;
-        std::cout << "*                                    *" << std::endl;
-        std::cout << "* Thank you for using the calculator!*" << std::endl;
-        std::cout << "*                                    *" << std::endl;
-        std::cout << "**************************************" << std::endl;
-    }
-}
